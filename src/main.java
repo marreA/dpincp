5bd5c509f42d5a5cead1f7cf9288e8d0353e0099
Andrea Visentin <andrea.visentin@insight-centre.org>
2018-07-17 14:57:18 +0100

    fixed memory error

--- src/main.java
+++ src/main.java
@@ -11,7 +11,7 @@ public class main {
     //Constants
     public static void main(String[] args) throws IOException {
 
-        args = new String[]{"correlated", "15", "5000", "20", "10", "CPgoogle", "MIP", "DP", "DPinCPgoogle", "DPinCPgooglev3"};
+        args = new String[]{"correlated", "15", "5000", "20", "10",  "MIP", "CPgoogle", "DP", "DPinCPgoogle"};
         InstanceGenerator benchmark = new StronglyCorrelated();;
         switch(args[0]) {
             case "correlated" :
@@ -26,50 +26,7 @@ public class main {
         int timeLimit = Integer.valueOf(args[4]);
 
         String[] solverNames =  Arrays.copyOfRange(args, 5, args.length);
-        ArrayList<KnapsackSolver> solvers = new ArrayList<KnapsackSolver>();
 
-        for (String name: solverNames) {
-            switch (name) {
-                case "CPchoco":
-                    solvers.add(new KnapsackBasicCP(n));
-                    break;
-                case "CPgoogle":
-                    solvers.add(new KnapsackBasicCPgoogle(n));
-                    break;
-                case "MIP":
-                    solvers.add(new KnapsackBasicMIP(n));
-                    break;
-                case "ConstraintChoco":
-                    solvers.add(new KnapsackConstraint(n));
-                    break;
-                case "DPinCPchoco":
-                    solvers.add(new KnapsackCPandDP(n));
-                    break;
-                case "DPinCPgoogle":
-                    solvers.add(new KnapsackCPandDPgoogle(n));
-                    break;
-                case "DPinCPgooglev2":
-                    solvers.add(new KnapsackCPandDPgooglev2(n));
-                    break;
-                case "DPinCPgooglev3":
-                    solvers.add(new KnapsackCPandDPgooglev3(n));
-                    break;
-                case "DP":
-                    solvers.add(new KnapsackDP(n));
-                    break;
-                case "DPv2":
-                    solvers.add(new KnapsackDPv2(n));
-                    break;
-                case "DPv2dic":
-                    solvers.add(new KnapsackDPv2dictionary(n));
-                    break;
-                case "DPinMIP":
-                    solvers.add(new KnapsackDPencodedMIP(n));
-                    break;
-                default:
-                    System.out.println("Solver not recognised");
-            }
-        }
 
 
 
@@ -83,13 +40,14 @@ public class main {
         int[] cost;
 
 
-        int solSize = solvers.size();
+        int solSize = solverNames.length;
+        KnapsackSolver solver = new KnapsackBasicCP(n);;
 
         long[][] results = new long[r][solSize];
 
         for (int s = 0 ; s < solSize; s++) {
-            System.out.print(solvers.get(s).getName() + ",");
-            solvers.get(s).setTimeLimit(timeLimit);
+            System.out.print(solverNames[s] + ",");
+
         }
         System.out.print("\n");
         for (int i = 0; i < r; i ++) {
@@ -103,16 +61,56 @@ public class main {
 //            System.out.println(v);
             for (int s = 0 ; s < solSize; s++) {
 //                System.out.println("Start solver " + solvers.get(s).getName());
-
+                switch (solverNames[s]) {
+                    case "CPchoco":
+                        solver = new KnapsackBasicCP(n);
+                        break;
+                    case "CPgoogle":
+                        solver = new KnapsackBasicCPgoogle(n);
+                        break;
+                    case "MIP":
+                        solver = new KnapsackBasicMIP(n);
+                        break;
+                    case "ConstraintChoco":
+                        solver = new KnapsackConstraint(n);
+                        break;
+                    case "DPinCPchoco":
+                        solver = new KnapsackCPandDP(n);
+                        break;
+                    case "DPinCPgoogle":
+                        solver = new KnapsackCPandDPgoogle(n);
+                        break;
+                    case "DPinCPgooglev2":
+                        solver = new KnapsackCPandDPgooglev2(n);
+                        break;
+                    case "DPinCPgooglev3":
+                        solver = new KnapsackCPandDPgooglev3(n);
+                        break;
+                    case "DP":
+                        solver = new KnapsackDP(n);
+                        break;
+                    case "DPv2":
+                        solver = new KnapsackDPv2(n);
+                        break;
+                    case "DPv2dic":
+                        solver = new KnapsackDPv2dictionary(n);
+                        break;
+                    case "DPinMIP":
+                        solver = new KnapsackDPencodedMIP(n);
+                        break;
+                    default:
+                        System.out.println("Solver not recognised");
+                }
+                solver.setTimeLimit(timeLimit);
 
                 long startTime = System.nanoTime();
 
-                solvers.get(s).solve(weight,cost,v);
+                solver.solve(weight,cost,v);
                 long estimatedTime = System.nanoTime() - startTime;
                 if (s == 0)
-                    solVal = solvers.get(s).getOptimalValue();
+                    solVal = solver.getOptimalValue();
 //                System.out.println("Risultato " + solvers.get(s).getOptimalValue() + "\tTime: " + estimatedTime  / 1000000 +"\n");
-                if (solVal - solvers.get(s).getOptimalValue() > 10) {// (solvers.get(s).getOptimalValue() == -1 ) {
+                if (solVal - solver.getOptimalValue() > 10) {// (solvers.get(s).getOptimalValue() == -1 ) {
                     System.out.print(-estimatedTime/1000000 + ",");
                     results[i][s] = -estimatedTime;
                 } else {
@@ -131,7 +129,7 @@ public class main {
         try (Writer writer = new BufferedWriter(new OutputStreamWriter(
                 new FileOutputStream("results_" + args[0] + "_"+ n + "_"+ r + "_"+ volume + ".csv"), "utf-8"))) {
             for (int s = 0 ; s < solSize; s++) {
-                writer.write(solvers.get(s).getName() + ",");
+                writer.write(solverNames[s] + ",");
             }
             writer.write("\n");
 
